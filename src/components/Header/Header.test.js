23334a7d818a7db12e119f03e1f92256c790b6d4
jai behl <jaibehl33@gmail.com>
2021-08-01 13:09:08 -0500

    test

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -2,10 +2,15 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import Header from './index';
 
+//setup function for header component
+const setUp = () => {
+    const component = shallow(<Header />);
+    return component;
+}
 
 describe('Header Component tests', () =>{
     it('It should render without errors', () =>{
-        const component = shallow(<Header />)
+        const component = setUp();
         const wrapper = component.find('.HeaderComponent')
     expect(wrapper.length).toBe(1);
 })
--- src/components/MainButton/MainButton.test.js
+++ src/components/MainButton/MainButton.test.js
@@ -2,14 +2,20 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import MainButton from './index';
 
+//main button test setup
+const setUp = () => {
+    const com = shallow(<MainButton />);
+    return com;
+}
+
 describe('Testing if the button renders', () => {
     it('Button parent container', ()=>{
-        const com = shallow(<MainButton />)
+        const com = setUp();
         const wrap = com.find('.center-div')
         expect(wrap.length).toBe(1);
     })
     it('btn child class',() => {
-        const comp = shallow(<MainButton />)
+        const comp = setUp();
         const wrapper = comp.find('.child')
         expect(wrapper.length).toBe(1)
     })
